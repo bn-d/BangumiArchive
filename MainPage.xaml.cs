aaa202e85b0e14ce52589350477158972217b32b
erebuxy <erebuxy@gmail.com>
2020-08-24 01:45:08 -0400

    Add to-watch view

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using BangumiArchive.UIModule;
+using System.Globalization;
 using Windows.UI.Xaml.Controls;
 
 namespace BangumiArchive
@@ -7,31 +8,48 @@ namespace BangumiArchive
     {
 
         private static Pivot static_MainPivot;
-        private static Frame static_MainPageFrame;
+        private static Frame static_MainFrame;
+        private static Frame static_ToWatchFrame;
 
         public MainPage()
         {
             InitializeComponent();
 
             static_MainPivot = MainPivot;
-            static_MainPageFrame = MainPageFrame;
+            static_MainFrame = MainPageFrame;
+            static_ToWatchFrame = ToWatchFrame;
 
             // Navigate to AnimeGridView
             MainPageFrame.Navigate(typeof(MainView));
+            ToWatchFrame.Navigate(typeof(ToWatchView));
             ScheduleFrame.Navigate(typeof(ScheduleView));
             StatisticsFrame.Navigate(typeof(StatisticsView));
         }
 
         public static void NavigateMainView()
         {
-            static_MainPageFrame.Navigate(typeof(MainView));
+            static_MainFrame.Navigate(typeof(MainView));
             static_MainPivot.SelectedIndex = 0;
+        }
+
+        public static void NavigateToWatchView()
+        {
+            static_ToWatchFrame.Navigate(typeof(ToWatchView));
+            static_MainPivot.SelectedIndex = 1;
         }
 
         public static void NavigateDetailView(SeriesIndex index)
         {
-            static_MainPageFrame.Navigate(typeof(DetailView), index);
-            static_MainPivot.SelectedIndex = 0;
+            if (index.Watched)
+            {
+                static_MainFrame.Navigate(typeof(DetailView), index);
+                static_MainPivot.SelectedIndex = 0;
+            }
+            else
+            {
+                static_ToWatchFrame.Navigate(typeof(DetailView), index);
+                static_MainPivot.SelectedIndex = 1;
+            }
         }
     }
 }
---
+++ UIModule/ToWatchView.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace BangumiArchive.UIModule
+{
+    /// <summary>
+    /// The to-watch list view
+    /// </summary>
+    public sealed partial class ToWatchView : Page
+    {
+        public ToWatchView()
+        {
+            InitializeComponent();
+            DataContextChanged += (s, e) => Bindings.Update();
+        }
+
+        /// <summary>
+        /// Add new series to to-watch list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private async void AddToWatchClickAsync(object sender, RoutedEventArgs e)
+        {
+            // Show input dialog to get the new Series name
+            var dialog = new InputDialog();
+            var result = await dialog.ShowAsync();
+
+            if (result != ContentDialogResult.Primary) return;
+
+            // Create new Series
+            string name = dialog.Text;
+            SeriesIndex si = DataManager.AddToWatchSeries(name);
+        }
+
+        /// <summary>
+        /// Move the current series to watched list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AddToMainClick(object sender, RoutedEventArgs e)
+        {
+            AppBarButton button = (AppBarButton)sender;
+            SeriesIndex idx = (SeriesIndex)button.DataContext;
+            var si = DataManager.MoveToWatchedSeries(idx.Index);
+            if (!MainView.IsFiltered)
+            {
+                MainView.StaticIndices.Add(si);
+            }
+
+            MainPage.NavigateDetailView(si);
+        }
+
+        /// <summary>
+        /// Delete the current series from to-watch list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void DeleteToWatchClick(object sender, RoutedEventArgs e)
+        {
+            AppBarButton button = (AppBarButton)sender;
+            SeriesIndex idx = (SeriesIndex)button.DataContext;
+            DataManager.RemoveToWatchSeries(idx.Index);
+        }
+
+        private void ToWatchItemClick(object sender, ItemClickEventArgs e) =>
+            MainPage.NavigateDetailView((SeriesIndex)e.ClickedItem);
+    }
+}
